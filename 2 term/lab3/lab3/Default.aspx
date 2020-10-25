c6594c620035ff105639c09072aadcfd218f8843
Aleksandr Yarchuk <learnmeasalex1912@ukr.net>
2020-10-25 12:34:22 +0200

    lab 3

---
+++ 2 term/lab3/lab3/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="lab3._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+
+    <asp:Label ID="PageTitle" runat="server" Text="Агенство по недвижимости"></asp:Label>
+    <br />
+    <br />
+
+    <asp:GridView ID="Worker" runat="server">
+    </asp:GridView>
+    <br />
+
+    <asp:GridView ID="TheProperty" runat="server">
+    </asp:GridView>
+</asp:Content>
---
+++ 2 term/lab3/lab3/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Xml.Linq;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab3
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet workersDataSet = new DataSet();
+            workersDataSet.ReadXml(MapPath("~/App_Data/Workers.xml"));
+            Worker.DataSource = workersDataSet;
+            Worker.DataBind();
+
+
+            DataSet thePropertyDataSet = new DataSet();
+            thePropertyDataSet.ReadXml(MapPath("~/App_Data/TheProperty.xml"));
+            TheProperty.DataSource = thePropertyDataSet;
+            TheProperty.DataBind();
+
+        }
+    }
+}
